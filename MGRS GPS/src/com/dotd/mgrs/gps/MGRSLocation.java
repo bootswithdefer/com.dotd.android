f57fd361627b4645928b20cc244d420e1195e851
Jesse DeFer <jdefer@gmail.com>
2012-09-19 10:36:32 -0700

    MGRSLocation extends Location

---
+++ MGRS GPS/src/com/dotd/mgrs/gps/MGRSLocation.java
@@ -0,0 +1,36 @@
+package com.dotd.mgrs.gps;
+
+import android.location.Location;
+
+public class MGRSLocation extends Location {
+
+	public MGRSLocation(String provider) {
+		super(provider);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MGRSLocation(Location l) {
+		super(l);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMGRSgzd() {
+		return "xx";
+	}
+	
+	public String getMGRSsquare() {
+		return "xx";
+	}
+	
+	public int getMGRSnorthing() {
+		return 0;
+	}
+	
+	public int getMGRSeasting() {
+		return 0;
+	}
+
+	public String toMGRSString() {
+		return getMGRSgzd() + getMGRSsquare() + " " + getMGRSnorthing() + " " + getMGRSeasting();
+	}
+}
