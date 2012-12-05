1d6900a5658705deb312fb44b8c47569358f60e4
jdefer <jdefer@gmail.com>
2012-12-04 18:04:01 -0700

    initial checkin

---
+++ Forensics/src/com/dotd/forensics/PhotoActivity.java
@@ -0,0 +1,119 @@
+package com.dotd.forensics;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class PhotoActivity extends Activity {
+	private SurfaceView preview = null;
+	private SurfaceHolder previewHolder = null;
+	private Camera camera = null;
+	private boolean inPreview = false;
+	private boolean cameraConfigured = false;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_photo);
+
+		preview = (SurfaceView) findViewById(R.id.preview);
+		previewHolder = preview.getHolder();
+		previewHolder.addCallback(surfaceCallback);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		camera = Camera.open();
+		if (camera == null)
+			camera = Camera.open(Camera.getNumberOfCameras() - 1);
+	}
+
+	@Override
+	public void onPause() {
+		if (inPreview) {
+			camera.stopPreview();
+		}
+
+		if (camera != null) {
+			camera.release();
+		}
+		camera = null;
+		inPreview = false;
+
+		super.onPause();
+	}
+
+	private Camera.Size getBestPreviewSize(int width, int height,
+			Camera.Parameters parameters) {
+		Camera.Size result = null;
+
+		for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+			if (result == null) {
+				result = size;
+			} else {
+				int resultArea = result.width * result.height;
+				int newArea = size.width * size.height;
+
+				if (newArea > resultArea) {
+					result = size;
+				}
+			}
+		}
+
+		return (result);
+	}
+
+	private void initPreview(int width, int height) {
+		if (camera != null && previewHolder.getSurface() != null) {
+			try {
+				camera.setPreviewDisplay(previewHolder);
+			} catch (Throwable t) {
+				Log.e("PreviewDemo-surfaceCallback",
+						"Exception in setPreviewDisplay()", t);
+				Toast.makeText(PhotoActivity.this, t.getMessage(),
+						Toast.LENGTH_LONG).show();
+			}
+
+			if (!cameraConfigured) {
+				Camera.Parameters parameters = camera.getParameters();
+				Camera.Size size = getBestPreviewSize(width, height, parameters);
+
+				if (size != null) {
+					parameters.setPreviewSize(size.width, size.height);
+					camera.setParameters(parameters);
+					cameraConfigured = true;
+				}
+			}
+		}
+	}
+
+	private void startPreview() {
+		if (cameraConfigured && camera != null) {
+			camera.startPreview();
+			inPreview = true;
+		}
+	}
+
+	SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+		public void surfaceCreated(SurfaceHolder holder) {
+			// no-op -- wait until surfaceChanged()
+		}
+
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+			initPreview(width, height);
+			startPreview();
+		}
+
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// no-op
+		}
+	};
+}
